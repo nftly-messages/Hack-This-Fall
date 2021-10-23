8713b3baf53173209a0c423828f44dd64c0c2037
chilaxan <chilaxan@gmail.com>
2021-10-23 18:14:54 -0400

    nav

--- users.py
+++ users.py
@@ -39,7 +39,9 @@ def get_posts(num=10, page=0, user=None):
         lposts = [posts[str(p)] for p in users[user]['posts']]
     else:
         lposts = list(posts.values())
-    return sorted(lposts[(page + 1) * num:(page + 1) * (num * 2)], key=getscore)[::-1]
+    lposts = sorted(lposts, key=getscore)[::-1]
+    index = page * num
+    return lposts[index: index + num]
 
 def make_post(payload, **data):
     if not data.get('text'):
