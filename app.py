079fb6512c7d681057159534fe2f03b55e775abe
chilaxan <chilaxan@gmail.com>
2021-10-23 11:40:38 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, make_response, redirect
+from sawo import createTemplate
+import requests
+import json
+import os
+from dotenv import load_dotenv
+import base64
+
+import users
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+app = Flask(__name__)
+app.static_folder = 'static'
+createTemplate("templates/partials", flask=True)
+
+def verifyToken(payload):
+    data = {
+        'user_id': payload['user_id'],
+        'verification_token': payload['verification_token']
+    }
+    resp = requests.post('https://api.sawolabs.com/api/v1/userverify/', data=data)
+    if resp.ok:
+        return resp.json()['user_valid']
+    else:
+        return False
+
+def get_payload():
+    b64payload = request.cookies.get('userID')
+    payload = json.loads(base64.b64decode(b64payload.encode())) if b64payload else {}
+    return payload
+
+def set_payload(payload):
+    b64payload = base64.b64encode(json.dumps(payload).encode())
+    resp = make_response({'status': 200})
+    resp.set_cookie('userID', b64payload)
+    return resp
+
+@app.route('/')
+def index():
+    payload = get_payload()
+    if payload and verifyToken(payload):
+        return render_template('index.html', userdata=users.get_userdata(payload), posts=users.get_posts())
+    else:
+        return render_template('index.html', posts=users.get_posts())
+
+@app.route('/user')
+def user():
+    payload = get_payload()
+    if payload and verifyToken(payload):
+        userdata = users.get_userdata(payload)
+        return render_template('user.html', userdata=userdata, posts=users.get_posts(user=payload['user_id']))
+    return redirect('/login_page')
+
+@app.route('/about')
+def about():
+    payload = get_payload()
+    if payload and verifyToken(payload):
+        return render_template('about.html', userdata=users.get_userdata(payload))
+    else:
+        return render_template('about.html')
+
+@app.route("/login_page")
+def login_page():
+    payload = get_payload()
+    if payload and verifyToken(payload):
+        return redirect('/user')
+    sawo = {
+        "auth_key": API_KEY,
+        "to": "login",
+        "identifier": "email"
+    }
+    return render_template("login.html", sawo=sawo)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    payload = json.loads(request.data)["payload"]
+    if verifyToken(payload):
+        return set_payload(payload)
+    return {'status': 404}
+
+@app.route("/new_post", methods=["POST"])
+def new_post():
+    payload = get_payload()
+    if not (payload and verifyToken(payload)):
+        return make_response({'failed': 'not authenticated'}, 403)
+    users.make_post(payload, **request.form)
+    return redirect('/user')
+
+@app.route("/vote/<pid>/<direction>", methods=['POST'])
+def vote(pid, direction):
+    payload = get_payload()
+    try:
+        pid = int(pid)
+    except:
+        return {'status': 404}
+    if direction == 'up':
+        if users.upvote(payload, pid) == -1:
+            return {'status': 404}
+        return {'status': 200}
+    elif direction == 'down':
+        if users.downvote(payload, pid) == -1:
+            return {'status': 404}
+        return {'status': 200}
+    return {'status': 404}
+
+@app.template_filter('get_score')
+def get_score(userdata):
+    return users.get_score(userdata)
+
+app.run()
---
+++ users.py
@@ -0,0 +1,93 @@
+import atexit
+import json
+
+with open('database/backup', 'r') as backup:
+    data = json.load(backup)
+    users = data.get('users') or {}
+    posts = data.get('posts') or []
+    pid = data.get('pid') or 0
+
+@atexit.register
+def backup():
+    data = {
+        'users': users,
+        'posts': posts,
+        'pid': pid
+    }
+    with open('database/backup', 'w') as backup:
+        json.dump(data, backup)
+
+def make_data(payload):
+    return {
+        "username": payload['identifier'],
+        "posts": []
+    }
+
+def getscore(post):
+    return len(post['up']) - len(post['down'])
+
+def get_userdata(payload):
+    user_id = payload.get('user_id')
+    if data := users.get(user_id):
+        return data
+    else:
+        return users.setdefault(user_id, make_data(payload))
+
+def get_posts(num=10, page=0, user=None):
+    if user:
+        lposts = [posts[p] for p in users[user]['posts']]
+    else:
+        lposts = posts
+    return sorted(lposts[page:page + num], key=getscore)[::-1]
+
+def make_post(payload, **data):
+    global pid
+    user_id = payload['identifier']
+    posts.append({
+        'pid': pid,
+        'user': user_id,
+        'text': data.get('text'),
+        'img': data.get('img'),
+        'up': [],
+        'down': []
+    })
+    userdata = get_userdata(payload)
+    userdata['posts'].append(pid)
+    pid += 1
+
+def upvote(payload, pid):
+    if not posts or pid < 0 or pid > len(posts):
+        return -1
+    post = posts[pid]
+    user_id = payload['identifier']
+    ups = post['up']
+    downs = post['down']
+    if user_id in ups:
+        ups.remove(user_id)
+        return 0
+    if user_id in downs:
+        downs.remove(user_id)
+    ups.append(user_id)
+    return 1
+
+def downvote(payload, pid):
+    if not posts or pid < 0 or pid > len(posts):
+        return -1
+    post = posts[pid]
+    user_id = payload['identifier']
+    ups = post['up']
+    downs = post['down']
+    if user_id in downs:
+        downs.remove(user_id)
+        return 0
+    if user_id in ups:
+        ups.remove(user_id)
+    downs.append(user_id)
+    return 1
+
+def get_score(userdata):
+    score = 0
+    for post_id in userdata['posts']:
+        post = posts[post_id]
+        score += len(post['up']) - len(post['down'])
+    return score
