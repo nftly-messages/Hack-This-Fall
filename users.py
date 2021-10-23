d00dccc601460c138f91fab66857066c5e15bfcd
chilaxan <chilaxan@gmail.com>
2021-10-23 12:40:44 -0400

    message

--- users.py
+++ users.py
@@ -99,7 +99,7 @@ def get_score(userdata):
 
 def get_post(pid):
     try:
-        breakpoint()
         return posts[int(pid)]
-    except:
+    except Exception as e:
+        print(e)
         return None
